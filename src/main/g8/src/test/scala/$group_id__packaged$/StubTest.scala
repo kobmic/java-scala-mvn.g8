37847c61ebe32d91e3b0d6c0e6219cd5e5b103fb
Michael Kober <michael.kober@jayway.com>
2014-05-06 12:47:07 +0200

    added back @RunWith annotation

--- src/main/g8/src/test/scala/$group_id__packaged$/StubTest.scala
+++ src/main/g8/src/test/scala/$group_id__packaged$/StubTest.scala
@@ -2,6 +2,10 @@ package $group_id$
 
 import org.scalatest._
 
+import org.scalatest.junit.JUnitRunner
+import org.junit.runner.RunWith
+
+@RunWith(classOf[JUnitRunner])
 class StubTest extends FunSpec with Matchers {	
 
   describe("your description here") {
